d59a0b9eee215356a82c941b5dd6604f9047ad12
David Philipson <david.philipson@gmail.com>
2023-10-14 10:57:47 -0700

    Use SWC in Jest tests (replace ts-jest)

--- circuits/jest.config.js
+++ circuits/jest.config.js
@@ -1,5 +1,4 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: "node",
+  transform: { "^.+\\.(t|j)sx?$": "@swc/jest" },
 };
