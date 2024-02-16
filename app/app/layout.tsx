791b6e28d7b513f3b4b26048a2b54d154fb9ac06
David Philipson <david.philipson@gmail.com>
2024-02-15 20:54:39 -0800

    Add background image

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import Script from "next/script";
 
 import AppWrapper from "./_components/appWrapper";
+import backgroundImage from "./_images/background.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={clsx("min-h-screen bg-primary", inter.className)}>
+      <body
+        className={clsx(
+          "min-h-screen bg-cover bg-fixed bg-center bg-no-repeat",
+          inter.className,
+        )}
+        style={{ backgroundImage: `url(${backgroundImage.src})` }}
+      >
         <AppWrapper>{children}</AppWrapper>
         <Script src="/snarkjs.min.js" />
       </body>
