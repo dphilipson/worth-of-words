d5cdb8abfb86ffddca6b18833abc5a236e693a6e
David Philipson <david.philipson@gmail.com>
2024-02-25 20:14:41 -0800

    Anvil deploy script now also deploys AA contracts
    That is, it deploys the entry point, the modular account impl and
    factory, and the multi-owner and session-key plugins.

--- contracts/script/DeployToAnvil.s.sol
+++ contracts/script/DeployToAnvil.s.sol
@@ -2,13 +2,15 @@
 pragma solidity ^0.8.21;
 
 import {Script} from "forge-std/Script.sol";
+import {AaDeploys} from "./AaDeploys.sol";
 import {BaseDeploy} from "./BaseDeploy.sol";
 
-contract DeployToAnvilScript is BaseDeploy {
+contract DeployToAnvilScript is BaseDeploy, AaDeploys {
     // Anvil key 0.
     uint256 private constant ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     function run() external {
+        deployAaContracts();
         runWithPrivateKey(ANVIL_PRIVATE_KEY, "anvilConstants.ts", "ANVIL", false);
     }
 }
--- contracts/script/deploy-to-anvil.sh
+++ contracts/script/deploy-to-anvil.sh
@@ -2,6 +2,16 @@
 
 set -ex
 
-forge script script/DeployToAnvil.s.sol:DeployToAnvilScript --fork-url http://localhost:8545 --broadcast -vvvv
+FACTORY_OWNER=0xddf32240b4ca3184de7ec8f0d5aba27dec8b7a5c
+
+# 10 eth
+cast rpc anvil_setBalance $FACTORY_OWNER 0x8ac7230489e80000
+cast rpc anvil_impersonateAccount $FACTORY_OWNER
+forge script script/DeployToAnvil.s.sol:DeployToAnvilScript \
+  --broadcast \
+  --fork-url http://localhost:8545 \
+  --sender $FACTORY_OWNER \
+  --unlocked
+cast rpc anvil_stopImpersonatingAccount $FACTORY_OWNER
 mkdir -p ../app/app/generated/
 cp out/anvilConstants.ts ../app/app/_generated/
